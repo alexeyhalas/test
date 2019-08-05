78cff57d571c82bb2e9536de6617beea1e5c4ae0
alexeyhalas <41819721+alexeyhalas@users.noreply.github.com>
2019-08-05 17:34:41 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,15 +1,40 @@
 import csv
+import xml.etree.cElementTree as ET
 
-price_list = []
+def main():
+    price_list = []
 
-with open ('pdsfeed.csv', newline = '') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if float(row['Price']) >= 50:
-            price_list.append([float(row['Price']), row['SKU'], row['URL']])
-price_list = sorted(price_list, key=lambda v:v[0])
-with open ('sorted.csv', 'w') as sorted:
-    writter=csv.writer(sorted)
-    writter.writerow(['Price', 'SKU', 'URL'])
-    for i in price_list:
-        writter.writerow(i)
+    with open('pdsfeed.csv') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            price = row['Price']
+            if float(price) >= 50:
+                price_list.append([float(price), row['SKU'], row['URL']])
+
+    price_list = sorted(price_list, key=lambda v: v[0])
+
+    with open ('sorted.csv', 'w') as f:
+        writter=csv.writer(f)
+        writter.writerow(['Price', 'SKU', 'URL'])
+        writter.writerows(price_list)
+    with open('pdsfeed.csv') as f:
+        root = ET.Element("shop")
+        doc = ET.SubElement(root, "offers")
+        reader = csv.DictReader(f)
+        for i in reader:
+            if (i['Brand'] == 'adidas' or i['Brand'] == 'Nike' or i['Brand'] == 'Assos') and i['Availability'] == 'in stock':
+                main_offer = ET.SubElement(doc, "offer", id=str(i['SKU']), availability='True')
+                ET.SubElement(main_offer, "name").text = i['Product Title']
+                ET.SubElement(main_offer, "url").text = i['URL']
+                ET.SubElement(main_offer, "vendor").text = i['Brand']
+
+            elif (i['Brand'] == 'adidas' or i['Brand'] == 'Nike' or i['Brand'] == 'Assos') and i['Availability'] == 'out of stock':
+                main_offer = ET.SubElement(doc, "offer", id=str(i['SKU']), availability='False')
+                ET.SubElement(main_offer, "name").text = i['Product Title']
+                ET.SubElement(main_offer, "url").text = i['URL']
+                ET.SubElement(main_offer, "vendor").text = i['Brand']
+
+        tree = ET.ElementTree(root)
+        tree.write("filename.xml")
+if __name__ == '__main__':
+   main()
