e8324520e0eb32ec22090665a4400333a3cb1dd7
alexeyhalas <41819721+alexeyhalas@users.noreply.github.com>
2019-08-07 14:42:16 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -3,25 +3,16 @@ import xml.etree.cElementTree as ET
 
 def main():
     price_list = []
-
     with open('pdsfeed.csv') as f:
         reader = csv.DictReader(f)
-        for row in reader:
-            price = row['Price']
-            if float(price) >= 50:
-                price_list.append([float(price), row['SKU'], row['URL']])
-
-    price_list = sorted(price_list, key=lambda v: v[0])
-
-    with open ('sorted.csv', 'w') as f:
-        writter=csv.writer(f)
-        writter.writerow(['Price', 'SKU', 'URL'])
-        writter.writerows(price_list)
-    with open('pdsfeed.csv') as f:
         root = ET.Element("shop")
         doc = ET.SubElement(root, "offers")
-        reader = csv.DictReader(f)
+
         for i in reader:
+            price = i['Price']
+            if float(price) >= 50:
+                price_list.append([float(price), i['SKU'], i['URL']])
+
             if (i['Brand'] == 'adidas' or i['Brand'] == 'Nike' or i['Brand'] == 'Assos') and i['Availability'] == 'in stock':
                 main_offer = ET.SubElement(doc, "offer", id=str(i['SKU']), availability='True')
                 ET.SubElement(main_offer, "name").text = i['Product Title']
@@ -36,5 +27,14 @@ def main():
 
         tree = ET.ElementTree(root)
         tree.write("filename.xml")
+
+        price_list = sorted(price_list, key=lambda v: v[0])
+        with open ('sorted.csv', 'w') as f:
+            writter=csv.writer(f)
+            writter.writerow(['Price', 'SKU', 'URL'])
+            writter.writerows(price_list)
+
+
 if __name__ == '__main__':
    main()
+
